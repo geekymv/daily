c30f90a80c39089eed59922626f4e3810eb6ad2f
geekymv <ym2011678@foxmail.com>
2017-03-14 21:56:37 +0800

    base service

--- src/main/java/com/geekymv/daily/base/BaseService.java
+++ src/main/java/com/geekymv/daily/base/BaseService.java
@@ -1,5 +1,10 @@
 package com.geekymv.daily.base;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public abstract class BaseService {
 
+	protected static final Logger logger = LoggerFactory.getLogger(BaseService.class);
+	
 }
